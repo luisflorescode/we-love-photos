707e42302a12a731d6c26c65aa1f2a37ab9f40ce
Luis Flores <luisfloresdev@gmail.com>
2019-07-13 05:06:44 -0500

    Se creó el componente del header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles/Header.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <span className="header-logo">❤ </span> We Love Photos
+      </header>
+    );
+  }
+}
+
+export default Header;
