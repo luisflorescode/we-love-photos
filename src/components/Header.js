3f14915cec8cc53705061d79623b81d27cd96d77
Luis Flores <luisfloresdev@gmail.com>
2019-07-14 23:27:03 -0500

    Estilos del Layout Se cambiaron los estilos del Layout y el Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Logo from "../images/logo.png";
 import "./styles/Header.css";
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <Link to={`/`}>
-          <span className="header-logo">❤</span>We Love Photos
+        <Link className="header" to={`/`}>
+          <img className="header-logo" src={Logo} alt="headerLogo" />
+          We Love Photos
         </Link>
       </header>
     );
